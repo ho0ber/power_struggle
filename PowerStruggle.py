9b99b8f1c2063325238c020a71538f1b4e21d277
Samuel Colburn <ho0ber@gmail.com>
2016-03-01 21:12:30 -0500

    Some minor cleanup

--- PowerStruggle.py
+++ PowerStruggle.py
@@ -16,6 +16,7 @@ class PowerStruggle:
     def OnServerInitialized(self):
         command.AddChatCommand('scores', self.Plugin, 'chat_scores')
         command.AddConsoleCommand("ps.scores", self.Plugin, "console_scores")
+
         for item in ItemManager.itemList:
             if item.name == self.Config["currency_res"]:
                 item.stackable = self.Config["currency_stack"]
@@ -25,7 +26,69 @@ class PowerStruggle:
         self.Config["currency_res"] = "battery_small.item"
         self.Config["currency_stack"] = 1000
 
+    ###########################
+    # Hooks
+
+    def OnPlayerInit(self, player):
+        dataObj = data.GetData("PowerStruggle")
+
+        if not "name_cache" in dataObj:
+            dataObj["name_cache"] = {}
+
+        dataObj["name_cache"][player.userID] = player.displayName
+        dataObj["id_cache"][player.displayName] = player.userID
+
+        data.SaveData("PowerStruggle")
+
+    def OnEntitySpawned(self, entity):
+        if hasattr(entity, "inventory") and type(entity.inventory) == ItemContainer:
+            item = ItemManager.CreateByName(self.Config["currency_item"])
+            item.MoveToContainer(entity.inventory, -1, False);
+
+            prefab_name = entity.ToString().split("/")[-1].split(".prefab")[0]
+            print "Adding {} to {} ({})".format(self.Config["currency_item"], prefab_name, entity.GetType())
+
+    ###########################
+    # Chat Command Handlers
+
+    def chat_scores(self, player, cmd, args):
+        """
+        Displays a list of scores by player display name to the requesting player
+        """
+        dataObj = data.GetData("PowerStruggle")
+        scores = self.calc_scores()
+
+        for user, score in self.calc_scores().iteritems():
+            rust.SendChatMessage(player, "{}: {}".format(dataObj["name_cache"][user], score), None, "76561198235146288")
+
+        if not scores:
+            rust.SendChatMessage(player, "No players have any {}".format(self.Config["currency_item"]), None, "76561198235146288")
+
+    ###########################
+    # Console Command Handlers
+
+    def console_scores(self, arg):
+        """
+        Displays a list of scores by player display name to the console
+        """
+        dataObj = data.GetData("PowerStruggle")
+
+        for user, score in self.calc_scores().iteritems():
+            print("{}: {}".format(dataObj["name_cache"][user], score))
+
+    ###########################
+    # Supporting Methods
+
     def calc_scores(self):
+        """
+        Calculates the count of the currency item for each player by steam ID
+
+        NOTE: Only items stored in containers created by a given player are
+        attributed to that player.
+
+        NOTE: This should be cached in the future, or otherwise decoupled from
+        the chat command, lest players learn to lag the server by abusing it.
+        """
         scores = {}
 
         for item in UnityEngine.Resources.FindObjectsOfTypeAll(StorageContainer):
@@ -37,6 +100,7 @@ class PowerStruggle:
                                 scores[item.OwnerID] = 0
                             scores[item.OwnerID] += i.amount
 
+        # # Checking inventories is still a WIP
         # for p in BasePlayer.activePlayerList:
         #     for i in p.inventory.containerMain.itemList:
         #         if i.info.name == "{}.item".format(self.Config["currency_res"]):
@@ -46,39 +110,3 @@ class PowerStruggle:
         #             scores[p.steamId] += i.amount
 
         return scores
-
-
-    def chat_scores(self,player,cmd,args):
-        dataObj = data.GetData("PowerStruggle")
-
-        scores = self.calc_scores()
-
-        for user, score in self.calc_scores().iteritems():
-            rust.SendChatMessage(player, "{}: {}".format(dataObj["name_cache"][user], score), None, "76561198235146288")
-
-        if not scores:
-            rust.SendChatMessage(player, "No players have any {}".format(self.Config["currency_item"]), None, "76561198235146288")
-
-    def console_scores(self, arg):
-        dataObj = data.GetData("PowerStruggle")
-        for user, score in self.calc_scores().iteritems():
-            print("{}: {}".format(dataObj["name_cache"][user], score))
-
-    # Hooks
-
-    def OnPlayerInit(self, player):
-        dataObj = data.GetData("PowerStruggle")
-        if not "name_cache" in dataObj:
-            dataObj["name_cache"] = {}
-        dataObj["name_cache"][player.userID] = player.displayName
-        dataObj["id_cache"][player.displayName] = player.userID
-        print "{}: {}".format(player.displayName, player.userID)
-        data.SaveData("PowerStruggle")
-
-    def OnEntitySpawned(self, entity):
-        if hasattr(entity, "inventory") and type(entity.inventory) == ItemContainer:
-            item = ItemManager.CreateByName(self.Config["currency_item"])
-            item.MoveToContainer(entity.inventory, -1, False);
-
-            prefab_name = entity.ToString().split("/")[-1].split(".prefab")[0]
-            print "Adding {} to {} ({})".format(self.Config["currency_item"], prefab_name, entity.GetType())
